9dae95b8cb8d4db83d69c97dac937cab3328851b
Benjamin <kipkoech9275@gmail.com>
2026-06-03 15:59:10 +0300

    feat(arrays): insertion algorithm

--- phase-1-fundamentals/arrays/implementations/insertion.js
+++ phase-1-fundamentals/arrays/implementations/insertion.js
@@ -5,7 +5,7 @@
  * Given the array {2, 4, 6, 8, 10}, write the algorithm steps to insert the value 5 at position K = 2.
  *
  * Time Complexity: O(n)
- * Space Complexity: O(1)
+ * Space Complexity: 0(1)
  */
 
 function insertion(arr, value, position) {
