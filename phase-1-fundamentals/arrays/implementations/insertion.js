cc67cb917fa42ad62fb7df015eea5376972a276b
Benjamin <kipkoech9275@gmail.com>
2026-06-03 15:58:01 +0300

    chore: folder restructure

--- phase-1-fundamentals/arrays/implementations/insertion.js
+++ phase-1-fundamentals/arrays/implementations/insertion.js
@@ -4,7 +4,27 @@
  * Description:
  * Given the array {2, 4, 6, 8, 10}, write the algorithm steps to insert the value 5 at position K = 2.
  *
- * Time Complexity: O(1)
+ * Time Complexity: O(n)
  * Space Complexity: O(1)
  */
 
+function insertion(arr, value, position) {
+    let n = arr.length;
+    let item = value;
+    let k = position;
+
+    console.log("Before:", arr);
+
+    let j = n;
+
+    while (j > k) {
+        arr[j] = arr[j - 1];
+        j--;
+    }
+
+    arr[k] = item;
+
+    console.log("After:", arr);
+}
+
+insertion([2, 4, 6, 8, 10], 5, 2);
