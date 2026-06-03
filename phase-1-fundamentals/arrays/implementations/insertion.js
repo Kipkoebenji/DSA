097ca46ef7ca54be8980b08bfce1c681761e8929
Benjamin <kipkoech9275@gmail.com>
2026-06-03 14:42:49 +0300

    chore: folder restructure

---
+++ phase-1-fundamentals/arrays/implementations/insertion.js
@@ -0,0 +1,10 @@
+/**
+ * Algorithm: This is Insertion
+ *
+ * Description:
+ * Given the array {2, 4, 6, 8, 10}, write the algorithm steps to insert the value 5 at position K = 2.
+ *
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
